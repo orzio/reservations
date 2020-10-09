25c497241b0239591ad755735186b3f1c535b979
krlartur <orzio488@gmail.com>
2020-10-09 23:39:17 +0200

    Add Room and Desk Repository

--- reservations - API/reservations/ProjectCalculator.Domain/Repositories/IRoomRepository.cs
+++ reservations - API/reservations/ProjectCalculator.Domain/Repositories/IRoomRepository.cs
@@ -1,11 +1,18 @@
 ﻿using ProjectCalculator.Api.Repositories;
+using Reservations.Core.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Reservations.Core.Repositories
 {
     public interface IRoomRepository:IRepository
     {
+        Task AddAsync(Room room);
+        Task DeleteAsync(Guid id);
+        Task UpdateAsync(Room room);
+        Task<IEnumerable<Room>> GetAllAsync();
+        Task<Room> GetAsync(Guid id);
     }
 }
